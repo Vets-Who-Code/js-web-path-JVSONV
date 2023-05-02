8fbc2aa12ef900587d2bba7051d26005bb3d63da
creatorjv <jvdevelops@gmail.com>
2023-05-02 18:44:03 -0400

    api issue first commit

--- issueWork/Async__issue/index.js
+++ issueWork/Async__issue/index.js
@@ -1,13 +1,26 @@
-const randomNum = () => Math.floor(Math.random() * (83 - 1) + 1);
+const makeElement = (tagname, innerText, classes) => {
+  const element = document.createElement(tagname);
+  element.classList = classes;
+  element.innerText = innerText;
+  return element;
+};
 
-async function getStarWarsCharacter() {
-  const data = await fetch(`https://swapi.dev/api/people/${randomNum()}`, {
-    method: "GET",
-    head: { "Content-type": "application/json" },
+const makeFilmList = (filmsArr) => {
+  const ulTag = document.createElement("ul");
+  ulTag.innerText = "Films";
+  filmsArr.forEach((film) => {
+    ulTag.append(makeElement("li", film.title, "movie"));
   });
-  const character = await data.json();
-  return character;
-}
+  return ulTag;
+};
+
+const makeCharacterInfo = (character, filmArr) => {
+  return [
+    makeElement("h1", character.name, "subject"),
+    makeElement("p", `Birth Year : ${character.birth_year}`, "subject"),
+    makeFilmList(filmArr),
+  ];
+};
 
 const getFilm = async (film) => {
   const filmData = await fetch(film, {
@@ -19,23 +32,47 @@ const getFilm = async (film) => {
   return movie;
 };
 
-const showData = () => {
-  getStarWarsCharacter().then((character) => {
-    console.log(character);
-    dataContainer.innerHTML = `<div class="character-name">Name:${
-      character.name
-    }</div><div>Height:${
-      character.height
-    }</div><ul class="character-films">${character.films
-      .map((film) => {
-        return `<li>${film}</li>`;
-      })
-      .join("")}</ul>`;
-  });
-};
-
 const apiCallButton = document.querySelector(".call-out");
 const dataContainer = document.querySelector(".data-container");
+
 apiCallButton.addEventListener("click", () => {
-  showData();
+  const randomNum = Math.floor(Math.random() * (83 - 1) + 1);
+  if (document.querySelector(".card")) {
+    document.querySelector(".card").remove();
+  }
+  dataContainer.append(
+    makeElement("p", "Loading Star Wars character info...", "loading")
+  );
+  new Promise((r) => setTimeout(r, 1000))
+    .then(() =>
+      fetch(`https://swapi.dev/api/people/${randomNum}`, {
+        method: "GET",
+        head: { "Content-type": "application/json" },
+      })
+    )
+    .then((data) => {
+      if (data.status === 404) throw new Error(data.status);
+      return data.json();
+    })
+    .then((characterData) => {
+      const filmData = async (characterData) => {
+        const allPromises = [];
+        characterData.films.forEach((film) => {
+          allPromises.push(getFilm(film));
+        });
+        const allFilms = await Promise.all(allPromises).catch((err) => {});
+        const createCharacterElement = (character, filmArr) => {
+          const characterCard = makeElement("div", "", "card")
+          const dataElement = makeCharacterInfo(character, filmArr);
+          dataContainer.append(characterCard);
+          characterCard.append(...dataElement);
+        };
+        createCharacterElement(characterData, allFilms);
+      };
+      filmData(characterData);
+    }).catch((r) => {
+      console.log(r);
+      dataContainer.append(makeElement("p", `An error has occured: ${r} `, "error"));
+    })
+    .finally(() => document.querySelector(".loading").remove());
 });
