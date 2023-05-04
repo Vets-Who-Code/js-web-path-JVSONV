677fd6a5c87288cf6da38c4a635df51a5ff613c3
creatorjv <jvdevelops@gmail.com>
2023-05-03 23:35:56 -0400

    Typescript & Parcel First Attempt

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+type objArr = { value: string }[];
+
+const objArrFunction = (arr: objArr, num: number) => {
+  for (let i = 0; i <= num; i++) {
+    let j = 0;
+    let randomLetters = "";
+    while (j <= num) {
+      randomLetters += String.fromCharCode(
+        Math.floor(Math.random() * (90 - 65) + 65) + 1
+      );
+      j++;
+    }
+    arr.push({ value: randomLetters });
+  }
+  return arr;
+};
+let randomNum: number = Math.floor(Math.random() * (5 - 1) + 1) + 1;
+let newArr: objArr = [];
+console.log(objArrFunction(newArr, randomNum));
