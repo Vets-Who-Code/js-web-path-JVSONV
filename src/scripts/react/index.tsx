d9b611d49fe9d0d0fca775bc89c1144f40d5f664
creatorjv <jvdevelops@gmail.com>
2023-05-05 14:58:33 -0400

    removed unnecessary ReactDOM import

--- src/scripts/react/index.tsx
+++ src/scripts/react/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const root = createRoot(document.getElementById("react-app") as HTMLElement);
