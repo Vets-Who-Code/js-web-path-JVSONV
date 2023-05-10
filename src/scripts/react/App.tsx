1cf1f0141284d85f048b0297cb38a85fc3b6ece7
JVSONV <112579344+JVSONV@users.noreply.github.com>
2023-05-09 20:22:18 -0400

    Typescript, React, Parcel (#25)
    * Typescript & Parcel First Attempt
    
    * second try
    
    * removed dist and cache
    
    * file changes and intergrated issue 6
    
    * tsconfig update
    
    * added change watching and native dom class in tsconfig for later use maybe
    
    * add typed return to typescript function
    
    * first commit
    
    * added .parcel-cache/ to .gitignore
    
    * lets test netlify
    
    * Netlify is good but had to change the ul appen location to reduce page rubberbanding on load
    
    * removed possiblity of null for divBackground element variable
    
    * removed unnecessary ReactDOM import
    
    * removed console.log in App.tsx

---
+++ src/scripts/react/App.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Button from "./Components/Button";
+import classes from "./App.module.css";
+
+const App = () => {
+  const [currentCount, setCurrentCount] = useState(0);
+  const divBackground = document.getElementById("react-app");
+
+  const addButtonHandler = () => {
+    setCurrentCount((prevCount) => prevCount+=1);
+  };
+
+  const subtractButtonHandler = () => {
+    setCurrentCount((prevCount) => (prevCount - 1 <= 0 ? 0 : prevCount-= 1));
+  };
+
+  const resetButtonHandler = () => {
+    setCurrentCount(0);
+  };
+
+  const backgroundColorHandler = () => {
+    divBackground!.classList.toggle("skittles");
+  };
+
+  return (
+    <div className={classes.app}>
+      <Button
+        innerText="Tast the rainbow"
+        onClickHandler={backgroundColorHandler}
+        class="rainbow"
+      />
+      <p>{currentCount}</p>
+      <div className={classes["button-container"]}>
+        <Button
+          innerText="Add"
+          onClickHandler={addButtonHandler}
+          class="add"
+        />
+        <Button
+          innerText="Subtract"
+          onClickHandler={subtractButtonHandler}
+          class="subtract"
+        />
+        <Button
+          innerText="Reset"
+          onClickHandler={resetButtonHandler}
+          class="reset"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
