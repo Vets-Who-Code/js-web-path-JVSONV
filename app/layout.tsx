9813b118190b1b94d34756568935ead54d614a44
creatorjv <jvdevelops@gmail.com>
2023-05-17 17:09:55 -0400

    added mobile contact button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Archivo } from "next/font/google";
 import Footer from "./components/Utility_Components/footer";
 import Modal from "./components/Utility_Components/modal";
 import Navigation from "./components/Utility_Components/navigation";
+import MobileContactButton from "./components/Utility_Components/mobileContact";
 import "./global.css";
 import ModalCtxProvider from "./store/modalCtxProvider";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Navigation />
           {children}
           <Modal />
+          <MobileContactButton />
           <Footer />
         </ModalCtxProvider>
       </body>
