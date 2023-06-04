2ac52dd309e3da059df723ad0c8cc6aefa094499
creatorjv <jvdevelops@gmail.com>
2023-06-04 11:44:19 -0400

    let see if these both wokr

--- pages/vetsWhoCode/index.tsx
+++ pages/vetsWhoCode/index.tsx
@@ -33,10 +33,11 @@ export const getServerSideProps: GetServerSideProps<{
   const path = require("path");
 
   console.log(process.cwd());
-  const dbPath = path.join(process.cwd(), "database.json");
+  // const dbPath = path.join(process.cwd(), "database.json");
+  const dbPath = "/database/database.json"
   console.log(dbPath)
   // const realPath = await fsPromises.realpath("./database.json")
-  const data = await fsPromises.readFile("database.json", "utf8");
+  const data = await fsPromises.readFile(dbPath, "utf8");
   const database = JSON.parse(data);
   return {
     props: {
